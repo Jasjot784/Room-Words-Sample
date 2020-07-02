d72c09127af4aca4a6073ffd566f427751cc6529
Jasjot Singh <jasjot784@gmail.com>
2020-07-02 19:21:23 +0530

    Deleting Single Word completed

--- app/src/main/java/com/jasjotsingh/roomwordssample/WordDao.java
+++ app/src/main/java/com/jasjotsingh/roomwordssample/WordDao.java
@@ -2,6 +2,7 @@ package com.jasjotsingh.roomwordssample;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -19,6 +20,9 @@ public interface WordDao {
 
     @Query("DELETE FROM word_table")
     void deleteAll();
+
+    @Delete
+    void deleteWord(Word word);
 
     @Query("SELECT * from word_table ORDER BY word ASC")
     LiveData<List<Word>> getAllWords();
--- app/src/main/java/com/jasjotsingh/roomwordssample/WordRepository.java
+++ app/src/main/java/com/jasjotsingh/roomwordssample/WordRepository.java
@@ -56,4 +56,20 @@ public class WordRepository {
     public void deleteAll()  {
         new deleteAllWordsAsyncTask(mWordDao).execute();
     }
+    private static class deleteWordAsyncTask extends AsyncTask<Word, Void, Void> {
+        private WordDao mAsyncTaskDao;
+
+        deleteWordAsyncTask(WordDao dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            mAsyncTaskDao.deleteWord(params[0]);
+            return null;
+        }
+    }
+    public void deleteWord(Word word)  {
+        new deleteWordAsyncTask(mWordDao).execute(word);
+    }
 }
